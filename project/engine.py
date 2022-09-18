4c0e32fa5965f1791000b68fd2051a9a6616f5eb
Victor Hugo <64938014+VictorHugoAmorim@users.noreply.github.com>
2022-09-18 19:13:53 -0300

    code recognition

--- project/engine.py
+++ project/engine.py
@@ -1,27 +1,25 @@
-import dlib
 import face_recognition as fr
-import numpy as np
-import cv2
 
 def reconhece_face(url_foto):
     foto = fr.load_image_file(url_foto)
     rostos = fr.face_encodings(foto)
-    if (len(rostos) > 0):
+    if(len(rostos) > 0):
         return True, rostos
-    else:
-        False,rostos
+    
+    return False, []
 
 def get_rostos():
-    #lista dos rostos
     rostos_conhecidos = []
     nomes_dos_rostos = []
-    nanda = reconhece_face("./project/img/nanda.png.png")
+
+    victor = reconhece_face("./project/img/victor.jpg")
+    if(victor[0]):
+        rostos_conhecidos.append(victor[1][0])
+        nomes_dos_rostos.append("victor")
+
+    nanda = reconhece_face("./project/img/nanda.png")
     if(nanda[0]):
         rostos_conhecidos.append(nanda[1][0])
-        nomes_dos_rostos.append('Nanda')
-
-    victor = reconhece_face("./project/img/victor.pngr.png")
-    if(victor[0]):
-        rostos_conhecidos.append(nanda[1][0])
-        nomes_dos_rostos.append('victor')
+        nomes_dos_rostos.append("nanda")
+    
     return rostos_conhecidos, nomes_dos_rostos
